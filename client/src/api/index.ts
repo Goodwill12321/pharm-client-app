4f82d83c8eb95c6eb7f595e0ea087b80333a8736
VK <kostenich.vv@gmail.com>
2025-04-21 23:08:14 +0400

    fix to relative path to api

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -2,4 +2,4 @@
 // Пример:
 // export async function getDebts() { ... }
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api';
+export const API_BASE_URL = '/api';
