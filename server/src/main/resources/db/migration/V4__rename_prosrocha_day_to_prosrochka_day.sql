fc763ed0875bef77167331c1309b25f6907c778f
Viktor Kostenich <kostenich.vv@gmail.com>
2025-07-31 22:29:13 +0300

    RENAME COLUMN Prosrocha_Day TO Prosrochka_Day

---
+++ server/src/main/resources/db/migration/V4__rename_prosrocha_day_to_prosrochka_day.sql
@@ -0,0 +1,4 @@
+-- V4__rename_prosrocha_day_to_prosrochka_day.sql
+-- Переименование колонки Prosrocha_Day в Prosrochka_Day в таблице debitorka
+
+ALTER TABLE debitorka RENAME COLUMN Prosrocha_Day TO Prosrochka_Day; 
